9ee20c115dfd7da40a1b1201ca6cc8509e5bae84
teebloc <102591864+teebloc@users.noreply.github.com>
2023-03-13 14:48:41 +0800

    Can now download PDFs of Questions without opening them, from Churned Results, as Admins

--- client/src/Components/Churn/Question.js
+++ client/src/Components/Churn/Question.js
@@ -1,4 +1,5 @@
 import API from "../../utils/API";
+import jsPDF from "jspdf";
 import {useState, useEffect} from "react";
 
 function Question(props) {
@@ -89,10 +90,69 @@ function Question(props) {
         try {
             const result = await API.get('/Questions/Upvotes/CheckUpvoted/' + QuestionID + '/' + Email)
 
-            setisUpvoted(result.data)
+            return result.data
         } catch(err) {
             console.log(err)
         }
+    }
+
+    //gets the more detailed Question info for downloading PDFs
+    async function GetQuestion(QuestionID) {
+        try {
+            const result = await API.get('/Questions/Get/' + QuestionID)
+            return result.data
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    async function DownloadPDF(event, QuestionID) {
+        event.stopPropagation();
+
+        //Get more detailed Question info for downloading PDFs
+        const Question = await GetQuestion(QuestionID)
+        console.log(Question)
+        const document = new jsPDF('p', 'mm', [297, 210])
+
+        var QuestionYPos = 0
+        //Add Question Images to PDF
+        for (var i=0; i<Question.QuestionImages.length; i++) {
+            const QuestionImage = Question.QuestionImages[i]
+            var image = new Image()
+            image.src = 'data:image/jpeg;base64,' + QuestionImage.QuestionIMGData
+            await image.decode()
+
+            if (i !== 0) {
+                if (image.height/image.width * 210 > (297 - QuestionYPos) ) {
+                    document.addPage()
+                    QuestionYPos = 0
+                }
+            }
+            document.addImage('data:image/jpeg;base64,' + QuestionImage.QuestionIMGData, 'JPEG', 0, QuestionYPos, 210, image.height/image.width * 210)
+            QuestionYPos += image.height/image.width * 210
+        }
+
+        document.addPage() //always start answers on a new page
+
+        var AnswerYPos = 0
+        //Add Answer Images to PDF
+        for (var i=0; i<Question.AnswerImages.length; i++) {
+            const AnswerImage = Question.AnswerImages[i]
+            var image = new Image()
+            image.src = 'data:image/jpeg;base64,' + AnswerImage.AnswerIMGData
+            await image.decode()
+
+            if (i !== 0) {
+                if (image.height/image.width * 210 > (297 - AnswerYPos) ) {
+                    document.addPage()
+                    AnswerYPos = 0
+                }
+            }
+            document.addImage('data:image/jpeg;base64,' + AnswerImage.AnswerIMGData, 'JPEG', 0, AnswerYPos, 210, image.height/image.width * 210)
+            AnswerYPos += image.height/image.width * 210
+        }
+
+        document.save('QuestionID' + QuestionID +'.pdf')
     }
     
     return (
@@ -130,6 +190,13 @@ function Question(props) {
             <br />
 
             Upvotes : {UpvoteCount}
+
+            {/* only show option to Download PDF of the question if logged in user is an Admin */}
+            {props.LoginData.AccType == 'Admin' ?
+                <button onClick={event => DownloadPDF(event, Question.questionid)}>Download PDF</button>
+            :
+                null
+            }
         </div>
     )
 }
